de9ae832a750078e49297997f2a964be55e7306c
processosbpa <processos@bomprincipioalimentos.com.br>
2025-02-06 14:24:14 -0300

    Update rigidos.js

--- rigidos.js
+++ rigidos.js
@@ -4,14 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
       const elements = {
         CONSERVAS: document.getElementById("Controle de materiais rígidos e cortantes - CONSERVAS"),
         CHOCOLATE: document.getElementById("Controle de materiais rígidos e cortantes - CHOCOLATE"),
-        "FRUTAS E POLPAS": document.getElementById("FRUTA E POLPA"),
+        FRUTAS E POLPAS: document.getElementById("FRUTA E POLPA"),
         PESAGEM: document.getElementById("SALA DE PESAGEM"),
         LEITE: document.getElementById("Controle de materiais rígidos e cortantes - LEITE"),
         ENVASE: document.getElementById("Controle de materiais rígidos e cortantes - ENVASE"),
         EMBALAGEM: document.getElementById("Controle de materiais rígidos e cortantes - EMBALAGEM"),
-        "SALA DE REPROCESSO": document.getElementById("Controle de materiais rígidos e cortantes - SALA DE REPROCESSO"),
-        "PESAGEM CHOCOLATE": document.getElementById("Controle de materiais rígidos e cortantes - PESAGEM CHOCOLATE"),
-        "PESAGEM CONSERVAS": document.getElementById("Controle de materiais rígidos e cortantes - PESAGEM CONSERVAS")
+        SALA DE REPROCESSO: document.getElementById("Controle de materiais rígidos e cortantes - SALA DE REPROCESSO"),
+        PESAGEM CHOCOLATE: document.getElementById("Controle de materiais rígidos e cortantes - PESAGEM CHOCOLATE"),
+        PESAGEM CONSERVAS: document.getElementById("Controle de materiais rígidos e cortantes - PESAGEM CONSERVAS")
       };
       const toggleElement = (element, show) => {
         element.style.display = show ? "block" : "none";
